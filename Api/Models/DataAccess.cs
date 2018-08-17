9a9df9d68ac57f2a73701c8c0b127e92d0fc604f
Morris <ngugimorry@gmail.com>
2018-08-17 12:29:39 +0300

    Add db Context

---
+++ Api/Models/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class EmployeeDbContext:DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext>options)
+            :base(options)
+        {
+        }
+
+        public class EmployeeDbContextFactory
+        {
+            public static EmployeeDbContext Create(string connectionString)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<EmployeeDbContext>();
+                optionsBuilder.UseSqlServer(connectionString);
+                var EmployeeDbContext = new EmployeeDbContext(optionsBuilder.Options);
+                return EmployeeDbContext;
+            }
+        }
+    }
+}
